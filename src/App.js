c68dbf25359a90f1e1be53c6980f1e18c05c1295
Owahed <owahedhossain@gmail.com>
2021-02-25 16:25:49 +0600

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[naoks, setNaoks] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data=>setNaoks(data))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <MovieCount></MovieCount>
+
+        {
+          naoks.map(nk => <Nayok name={nk.name} key={nk.id}></Nayok>)
+        }
+
+        {/* <Nayok name='roky'></Nayok>
+        <Nayok name='rakib'></Nayok>
+        <Nayok name='nisad'></Nayok>
+        <Nayok name='rony'></Nayok> */}
+        
       </header>
     </div>
   );
+}
+
+function MovieCount() {
+  const[count,setCount] =useState(0);
+  
+  const handelClick =()=>setCount(count+1);
+  return(
+    <div>
+      <button onClick={handelClick}>Add Movies</button>
+      <h3>Number of Movies : {count} </h3>
+    </div>
+  )
+}
+
+
+function Nayok(props) {
+ const style={
+    backgroundColor:'gray',
+    padding:'15px',
+    margin:'15px'
+  }
+  return(
+    <div style={style}>
+      <h3>My name : {props.name}</h3>
+      <h5>I have done </h5>
+    </div>
+  )
 }
 
 export default App;
